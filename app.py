5a3c2db8802585b08b08b159443175f081a8381f
ShetManoj <manojrsnet@gmail.com>
2024-07-18 14:14:22 +0100

    Modified python file

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+from werkzeug.urls import url_quote_plus  # Import url_quote_plus instead of url_quote
 import requests
 
 app = Flask(__name__)
@@ -10,8 +11,8 @@ def home():
 @app.route('/weather', methods=['POST'])
 def weather():
     city = request.form['city']
-    api_key = 'your_openweathermap_api_key'
-    weather_url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}'
+    api_key = 'your_openweathermap_api_key'  # Replace with your actual OpenWeatherMap API key
+    weather_url = f'http://api.openweathermap.org/data/2.5/weather?q={url_quote_plus(city)}&appid={api_key}'  # Use url_quote_plus here
     response = requests.get(weather_url)
     weather_data = response.json()
     if weather_data['cod'] == 200:
